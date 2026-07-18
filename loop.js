51673fc447dd1cc5ab670bdc8c0ad6b94e40d884
asm-saim <saim777m@gmail.com>
2026-07-18 11:12:02 +0600

    first commit

---
+++ loop.js
@@ -0,0 +1,30 @@
+// Task 1 — Print "Hello" Five Times
+// Write a for loop that prints "Hello" 5 times.
+// // Your code here
+
+for (let i = 1; i <= 5; i++) {
+    console.log("Hello")
+}
+
+// Task 2 — Print Numbers 1 to 10
+// Write a for loop that prints numbers from 1 to 10.
+// // Your code here
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+
+// Task 3 — Countdown with While Loop 
+// Write a while loop that prints numbers from 10 down to 1.
+// Your code here
+let number = 10;
+while (number >= 1) {
+    console.log(number)
+    number--;
+}
+
+
+
+// Task 4 — Repeat a Message
+// Using a while loop, print "Loop চলছে" exactly 7 times.
+// // Your code here
