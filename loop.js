e5f9a71387581798dcc9d0bf355cd6b8d65667a9
asm-saim <saim777m@gmail.com>
2026-07-18 13:44:22 +0600

    used break in for loop

--- loop.js
+++ loop.js
@@ -83,3 +83,9 @@ console.log("Total sum of even number from 2 to 50 is: ", store)
 // Task 10 — Using Break
 // Write a for loop from 1 to 50 that stops completely (break) as soon as it reaches a number greater than 30.
 // // Your code here
+for (let i = 1; i <= 50; i++) {
+    if (i > 30) {
+        break;
+    }
+    console.log(i)
+}
